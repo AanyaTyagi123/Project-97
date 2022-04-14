dad01f83a486b30cbeb3105c62655c8a489e2ec9
AanyaTyagi123 <89974904+AanyaTyagi123@users.noreply.github.com>
2022-04-14 16:12:23 +0530

    Add files via upload

---
+++ alphabet.js
@@ -0,0 +1,78 @@
+function a_(){
+    document.getElementById("a").play();
+}
+function b(){
+    document.getElementById("b").play();
+}
+function c(){
+    document.getElementById("c").play();
+}
+function d(){
+    document.getElementById("d").play();
+}
+function e(){
+    document.getElementById("e").play();
+}
+function f(){
+    document.getElementById("f").play();
+}
+function g(){
+    document.getElementById("g").play();
+}
+function h(){
+    document.getElementById("h").play();
+}
+function i(){
+    document.getElementById("i").play();
+}
+function j(){
+    document.getElementById("j").play();
+}
+function k(){
+    document.getElementById("k").play();
+}
+function l(){
+    document.getElementById("l").play();
+}
+function m(){
+    document.getElementById("m").play();
+}
+function n(){
+    document.getElementById("n").play();
+}
+function o(){
+    document.getElementById("o").play();
+}
+function p(){
+    document.getElementById("p").play();
+}
+function q(){
+    document.getElementById("q").play();
+}
+function r(){
+    document.getElementById("r").play();
+}
+function s(){
+    document.getElementById("s").play();
+}
+function t(){
+    document.getElementById("t").play();
+}
+function u(){
+    document.getElementById("u").play();
+}
+function v(){
+    document.getElementById("v").play();
+}
+function w(){
+    document.getElementById("w").play();
+}
+function x(){
+    document.getElementById("x").play();
+}
+function y(){
+    document.getElementById("y").play();
+}
+function z(){
+    document.getElementById("z").play();
+}
---
+++ main.js
@@ -0,0 +1,10 @@
+function alpha()
+{
+    window.location = "alphabet.html";
+    console.log("redirecting to 'alphabet.html'");
+}
+function number()
+{
+    window.location = "number.html";
+    console.log("redirecting to 'number.html'");
+}
